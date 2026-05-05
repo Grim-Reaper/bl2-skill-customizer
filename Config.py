9d852a270f00f29786ae833c8e5efffbb2c6722e
Grim Reader <grimreaperpro@gmail.com>
2026-05-05 11:17:21 +0530

    Adding a default value.

--- Config.py
+++ Config.py
@@ -18,10 +18,19 @@ if not _IsLoaded:
             for skillSet in skillSetList:
                 Choices.append(skillSet["Name"])
                 Configs[skillSet["Name"]] = skillSet["Skills"]
-                if "Active" in skillSet and skillSet["Active"]:
-                    StartingValue = skillSet["Name"]
     else:
         with open(dir + "\\data.json", "w") as f:
             f.write("[]")
             _IsLoaded = True
+
+if StartingValue is None:
+    dir = os.path.dirname(os.path.realpath(__file__))
+    if os.path.isfile(dir + "\\data.json"):
+        with open(dir + "\\settings.json", "r") as f:
+            settings = json.loads(f.read())
+            if settings["Options"] is not None and settings["Options"]["Select Skill Set"] is not None:
+                StartingValue = settings["Options"]["Select Skill Set"]
+
+if StartingValue is None:
+    StartingValue = "Insane Gaige"
 
